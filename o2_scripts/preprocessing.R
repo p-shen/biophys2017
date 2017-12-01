b89094e44daf438d558678bc8c061ad50590d0d4
Peter Shen <peter.shen@aimsio.com>
2017-12-01 10:53:27 -0500

    Write out essential and non-essential genes
    The essential genes and non-essential genes are written out to separate
    data files instead of keeping the column ids.

--- o2_scripts/preprocessing.R
+++ o2_scripts/preprocessing.R
@@ -2,7 +2,8 @@ library(dplyr)
 library(tidyverse)
 
 # to test this locally, change this to the sample file
-gx <- data.table::fread("../GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_tpm.gct", sep = "\t", skip=2) # skip headers
+#gx <- data.table::fread("../sample.csv", sep = "\t", skip=2) # skip headers
+gx <- data.table::fread("../GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_tpm.gct", sep = "\t", skip=2) # skip
 attr <- read.csv("../GTEx_v7_Annotations_SampleAttributesDS.txt", sep="\t")
 
 gx_by_gene <- as.data.frame(t(gx[,3:ncol(gx)])) # select only expression data and transpose
@@ -17,9 +18,18 @@ gx_by_gene <- attr %>%
 ess_gene_list <- data.table::fread("../essential_genes.csv", drop = 1)$x
 noness_gene_list <- data.table::fread("../nonessential_genes.csv", drop = 1)$x
 
-# get the matched essential genes to the column ids
+# Get the essential and non-essential genes column IDs
 matched_ess <- match(ess_gene_list, colnames(gx_by_gene))
-write.table(matched_ess, file="../essential_gene_cols.csv", sep="\t")
+names(matched_ess) <- ess_gene_list
+matched_ess <- matched_ess[!is.na(matched_ess)] # remove NA (no matching genes)
 
 matched_non_ess <- match(noness_gene_list, colnames(gx_by_gene))
-write.table(matched_non_ess, file="../nonessential_gene_cols.csv", sep="\t")
+names(matched_non_ess) <- noness_gene_list
+matched_non_ess <- matched_non_ess[!is.na(matched_non_ess)] # remove NA (no matching genes)
+
+# Get the expression data for the essential and non-essential genes and write out
+ess_gx <- gx_by_gene[,c(1,2,matched_ess)]
+write.table(ess_gx, file="../essential_gene_cols.csv", sep="\t")
+
+non_ess_gx <- gx_by_gene[,c(1,2,matched_non_ess)]
+write.table(non_ess_gx, file="../nonessential_gene_cols.csv", sep="\t")
